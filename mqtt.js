516c769c93ca9bc498b946a57737ba7dae8ff437
Mạnh Hoàng <manhhoangbk2@gmail.com>
2018-12-04 08:41:19 +0700

    doneMqtt

--- mqtt.js
+++ mqtt.js
@@ -17,12 +17,26 @@ var options = {
 var client;
 const TOPIC_LISTEN_AUTHENCATICAION = "nct_authentication";
 const TOPIC_PUBLISH_AUTHENCATICAION = "nct_authentication_result_";
-const TOPIC_BROKER = "nct_collect_";
+const TOPIC_BROKER = "nct_collect_12";
 
 var MQTT = {
-    
+
     initMQTT: function(){
         client = mqtt.connect('mqtt://iot.eclipse.org', options);
+        client.on('message', (topic, message, packet)  => {
+            console.log('topic ', topic)
+            if(!topic){
+                console.log('err')
+            } else{
+                message = JSON.parse(message.toString());    
+                if(topic === TOPIC_LISTEN_AUTHENCATICAION){
+                    this.onHanderAuthentication(message)
+                } else if(topic === TOPIC_BROKER){
+                    this.onHanderCollect(message)
+                }
+            }
+            
+        });
     },
 
     onSubcribeCollect: function(){
@@ -31,19 +45,6 @@ var MQTT = {
         }
         client.on('connect', function() { // When connected
             client.subscribe(TOPIC_BROKER, function() {
-                client.on('message', function(topic, message, packet) {
-                    message = JSON.parse(message.toString());
-                        var key_device = message.key_device;
-                        console.log('onSubcribeCollect ', message)
-                        DeviceAuthentication.getDeviceAuthenticationByKey(key_device, function(err, row){
-                            if(err){
-                            } else{
-                                console.log('DeviceAuthentication okiii ', row)
-                                // SenserData.saveMultiSenser(message, function(err, row){
-                                // })
-                            }
-                        })
-                });
             });
         });
     },
@@ -54,30 +55,43 @@ var MQTT = {
         }
         client.on('connect', function() { // When connected
             client.subscribe(TOPIC_LISTEN_AUTHENCATICAION, function() {
-                client.on('message', function(topic, message, packet) {
-                    message = JSON.parse(message.toString());
-                    DeviceAuthentication.getDeviceAuthentication(message, function(err, result){
-                        var objReturn ={ 
-                            result: 'PASS', 
-                            cycle: 0.5,
-                            key: 0
-                        } 
-                        if(err){
-                            objReturn.key = -2;
-                        } else{
-                            result = result[0];
-                            if(result){
-                                objReturn.key = result.key_device;
-                            } else{
-                                objReturn.key = -1;
-                            }
-                        }
-                        console.log('message ', objReturn)
-                        client.publish(TOPIC_PUBLISH_AUTHENCATICAION+message.id, JSON.stringify(objReturn), function() {
-                            console.log("Message is published ");
-                        });
-                    });
-                });
+            });
+        });
+    },
+
+    onHanderCollect: function(message){
+        var key_device = message.key_device;
+        console.log('onSubcribeCollect ', message)
+        DeviceAuthentication.getDeviceAuthenticationByKey(key_device, function(err, row){
+           if(err){
+            } else{
+               console.log('DeviceAuthentication okiii ', row)
+                SenserData.saveMultiSenser(message, function(err, row){
+                })
+            }
+        })
+    },
+
+    onHanderAuthentication: function(message){
+        DeviceAuthentication.getDeviceAuthentication(message, function(err, result){
+            var objReturn ={ 
+                result: 'PASS', 
+                cycle: 0.5,
+                key: 0
+            } 
+            if(err){
+                objReturn.key = -2;
+            } else{
+                result = result[0];
+                if(result){
+                    objReturn.key = result.key_device;
+                } else{
+                    objReturn.key = -1;
+                }
+            }
+            console.log('message ', objReturn)
+            client.publish(TOPIC_PUBLISH_AUTHENCATICAION+message.id, JSON.stringify(objReturn), function() {
+                console.log("Message is published ");
             });
         });
     },
